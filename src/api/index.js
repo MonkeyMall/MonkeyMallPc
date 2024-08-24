9520e23d3c710c4279bcabf47b7214b2c1472650
songyanbin <598349146>
2024-08-24 16:42:02 +0800

    评论列表

---
+++ src/api/index.js
@@ -0,0 +1,57 @@
+import request from "@/utils/request";
+import config from "@/config";
+//
+export const urlMap = {
+  userLogin: "/api/user/login",
+  userInfo: "/api/user/info",
+  companyList: "/api/company/list",
+  ridiculeList: "/api/ridicule/list",
+  ridiculeCommentList: "/api/comment/commentList"
+};
+// console.log('环境', process.env)
+const BASE = process.env.NODE_ENV === 'development' ? config.BASE_API_BANNER : process.env.VUE_APP_BASE_API_BANNER
+// 批量增加url前缀
+Object.keys(urlMap).forEach(key => {
+    urlMap[key] = urlMap[key];
+    // urlMap[key] = BASE + urlMap[key];
+});
+
+export function userInfo(params) {
+  return request({
+    url: urlMap['userInfo'],
+    method: "get",
+    params
+  });
+}
+
+export function userLogin(para) {
+  return request({
+    url: urlMap["userLogin"],
+    method: "post",
+    data: para
+  });
+}
+
+export function companyList(params) {
+  return request({
+      url: urlMap['companyList'],
+      method: "get",
+      params
+  });
+}
+
+// 侃言列表
+export function getRidiculeList(params) {
+  return request({
+    url: urlMap['ridiculeList'],
+    method: 'get',
+    params
+  })
+}
+export function ridiculeCommentList(params) {
+  return request({
+    url: urlMap['ridiculeCommentList'],
+    method: 'post',
+    data: params
+  })
+}
