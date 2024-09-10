07f7eca46ab368290b21ec0cd555820dc1408679
songyanbin <598349146>
2024-09-10 14:00:34 +0800

    优化

---
+++ src/views/Mine/MyLiuyan.vue
@@ -0,0 +1,79 @@
+<template>
+  <ContainerLayout :isHeader="true" :isFooter="true">
+    <template slot="main">
+      <div class="PC-mine">
+        <div class="user-info">
+          <img src="@/static/imgs/logo.png">
+          <div class="user-name">{{ userInfo.username }}</div>
+        </div>
+        <div class="mine-list">
+          <myRidiculePc />
+        </div>
+      </div>
+    </template>
+  </ContainerLayout>
+</template>
+
+<script>
+import ContainerLayout from "@/components/containerComponent";
+import myRidiculePc from "./components/myLiuyanPc";
+import { getItem } from "@/utils/storage";
+
+export default {
+  name: "Mine",
+  components: {
+    ContainerLayout,
+    myRidiculePc,
+  },
+  data() {
+    return {
+      userInfo: getItem('userInfo'),
+    };
+  },
+  created() {
+  },
+  mounted() {},
+  computed: {},
+  methods: {
+    
+  }
+};
+</script>
+<style lang="scss" scoped>
+.PC-mine {
+  width: 1200px;
+  margin: 0px auto;
+  display: flex;
+  gap: 20px;
+  .user-info {
+    width: 150px;
+    height: 150px;
+    padding: 20px;
+    border-radius: 10px;
+    background-color: #fff;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    img {
+      width: 100px;
+      height: 100px;
+      border-radius: 50%;
+    }
+    .user-name {
+      margin-top: 20px;
+      font-size: 18px;
+      font-weight: bold;
+      color: #333;
+    }
+  }
+  .mine-list {
+    flex: 1;
+    padding: 20px;
+    border-radius: 10px;
+    background-color: #fff;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  }
+}
+  
+</style>
---
+++ src/views/Mine/components/myLiuyanPc.vue
@@ -0,0 +1,274 @@
+<template>
+  <div class="PC-ridicule">
+    <ul class="web-left" v-if="list.length > 0">
+      <li v-for="(item, index) in list" 
+        :key="index" 
+        :class="[lookIndex === index ? 'active' : '']"
+      >
+        <p class="title">{{ item.desc }}</p>
+      </li>
+      <div class="pageFy">
+        <pageNum 
+          :currentPage="currentPage" 
+          :total="total" 
+          @changePage="handleCurrentChange"
+        />
+      </div>
+    </ul>
+    <div v-else class="empty">
+      <p>您暂无给作者留言</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import {
+  liuyanList,
+} from "@/api/index";
+import windowRight from "@/components/windowRight/windowRight";
+import pageNum from "@/components/pageNum/index.vue";
+import { mapState, mapMutations } from "vuex";
+
+var that = null;
+export default {
+  name: "Ridicule",
+  components: {
+    windowRight,
+    pageNum
+  },
+  data() {
+    return {
+      list: [],
+      drawer: false,
+      commentInfo: null,
+      lookIndex: '',
+      plText: '',
+      fhText: '',
+      id: '', // 当前操作的文章id
+      currentPage: 1,
+      total: 0,
+      query: {
+        page: 1,
+      }
+    };
+  },
+  created() {
+    this.getData()
+  },
+  mounted() {},
+  computed: {
+    ...mapState(['searchText'])
+  },
+  watch: {
+    
+  },
+  methods: {
+    // 评论列表
+    async getData() {
+      let {data, count} = await liuyanList(this.query);
+      data.map(item =>{
+        item.isShowMore = false
+      })
+      this.list = data || []
+      this.total = count
+      console.log('list:', data);
+    },
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+
+.PC-ridicule {
+  width: 100%;
+  margin: 0px auto;
+  display: flex;
+  gap: 20px;
+  .empty {
+    width: 100%;
+    text-align: center;
+    padding: 20px;
+    color: #8491a5;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 10px;
+    .creatTk {
+      width: 100px;
+      line-height: 30px;
+      text-align: center;
+      border-radius: 30px;
+      background: #2bc7a0;
+      margin-top: 10px;
+      color: #fff;
+      cursor: pointer;
+    }
+  }
+  .PC-drawer {
+    position: relative;
+    height: 100vh;
+    ::v-deep .el-drawer__header {
+      font-size: 18px;
+      font-weight: 600;
+      color: #333;
+      text-align: left;
+    } 
+    ::v-deep .el-drawer__body {
+      font-size: 14px;
+      text-align: left;
+    }
+    ::v-deep .demo-drawer__footer{
+      position: absolute;
+      bottom: 0;
+      width: 100%;
+      padding: 20px;
+      background: #fff;
+    }
+    .pl-input {
+      display: flex;
+      gap: 10px;
+    }
+    .pl-list {
+      margin-bottom: 80px;
+      .select-item {
+        margin: 20px;
+        color: #000;
+        font-size: 16px;
+      }
+      ul {
+        li {
+          padding: 10px 0;
+          &:not(:last-child) {
+            border-bottom: 1px solid #f8f8fa;
+          }
+          .anthor {
+            margin-bottom: 5px;
+            font-size: 12px;
+            color: #8491a5;
+            display: flex;
+            gap: 10px;
+            align-items: center;
+            .person {
+              font-size: 15px;
+            }
+          }
+        }
+      }
+      ul {
+        li {
+          padding: 10px 0;
+          border-bottom: 1px solid #f8f8fa;
+          .anthor {
+            margin-bottom: 5px;
+            font-size: 12px;
+            color: #8491a5;
+            display: flex;
+            gap: 10px;
+            align-items: center;
+            .person {
+              font-size: 15px;
+              font-weight: bold;
+              text-decoration: none;
+              color: rgb(55, 58, 64);
+              overflow: hidden;
+              white-space: nowrap;
+              text-overflow: ellipsis;
+            }
+            .pl-author {
+              background: #f8f8fa;
+              padding: 2px 8px;
+              border-radius: 5px;
+              color: #8491a5;
+            }
+          }
+          .pl-cons {
+            margin-bottom: 5px;
+          }
+          .pl-bar {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            &-btn {
+              color: rgb(132, 145, 165);
+              cursor: pointer;
+              &:hover {
+                color:rgb(57, 62, 71);
+              }
+            }
+            &-time {
+              color: #8491a5;
+            }
+          }
+          .hf-input {
+            margin-top: 5px;
+          }
+        }
+      } 
+      .empty {
+        text-align: center;
+        padding: 20px;
+        color: #8491a5;
+        margin-top: 60px;
+      }
+    }
+  }
+  .pageFy {
+    margin-top: 20px;
+  }
+  ul {
+    background: #fff;
+    padding: 20px;
+    flex: 1;
+    li {
+      padding: 10px;
+      cursor: pointer;
+      &:not(:last-child) {
+        border-bottom: 1px solid #f8f8fa;
+      }
+      &.active {
+        border: 1px solid rgba(0, 186, 173, .6);
+        border-radius: 8px;
+      }
+      .title{
+        font-size: 18px;
+        font-weight: 600;
+        text-align: left;
+        margin-bottom: 5px;
+      }
+      .content{
+        font-size: 15px;
+        color: #191b1f;
+        text-align: left;
+        .more{
+          color: #09408e;
+          cursor: pointer;
+        }
+      }
+      .content-bar {
+        display: flex;
+        gap: 15px;
+        &-item {
+          font-size: 14px;
+          color: #8491a5;
+          cursor: pointer;
+          margin-top: 5px;
+          .icon{
+            fill: #8491a5;
+          }
+          &:hover {
+            color: rgba(0, 186, 173, .6);
+            .icon{
+              fill: rgba(0, 186, 173, .6);
+            }
+          }
+        }
+      }
+    }
+  }
+  .web-right {
+    width: 240px;
+    min-height: 100%;
+    background: #fff;
+  }
+}
+</style>
